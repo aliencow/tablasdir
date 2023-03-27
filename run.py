811987f13efc5c7a7a7fc53a8bb988210322b805
juan.nouche <juan.nouche@antaruxa.com>
2023-03-27 17:42:30 +0200

    updated two things

--- run.py
+++ run.py
@@ -1,3 +1,53 @@
-from tables import app
+# print("Ingrese dos números para sumar.")
+# input("numero uno")
+# num1 = float(input("Ingrese el primer número: "))
+# num2 = float(input("Ingrese el segundo número: "))
+# resultado = num1 + num2
+# print("El resultado de la suma es:", resultado)
 
-app.run(debug=True)
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
+
+class Ventana(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.inicializar_ui()
+
+    def inicializar_ui(self):
+        
+
+        # Crear widgets
+        etiqueta_nombre = QLabel("Ingrese su nombre:")
+        self.entrada_nombre = QLineEdit()
+        boton_guardar = QPushButton("Guardar")
+
+        # Conectar el botón a un manejador de eventos
+        boton_guardar.clicked.connect(self.guardar_nombre)
+
+        # Diseñar la ventana
+        layout = QVBoxLayout()
+        layout.addWidget(etiqueta_nombre)
+        layout.addWidget(self.entrada_nombre)
+        layout.addWidget(boton_guardar)
+
+        self.setLayout(layout)
+        self.setWindowTitle("Guardar Nombre")
+
+    def guardar_nombre(self):
+        nombre = self.entrada_nombre.text()
+
+        # Guardar el nombre en un archivo
+        with open("nombres.txt", "a") as archivo:
+            archivo.write(nombre + "\n")
+
+        # Limpiar la entrada
+        self.entrada_nombre.setText("")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ventana = Ventana()
+    ventana.show()
+    sys.exit(app.exec_())
